30841f051208ee8e7fa72edf8c2040b18b853bcc
Joseph C <josephalexcobian@gmail.com>
2023-09-05 00:53:16 -0700

    Add a newline after every phrase is typed, and change the phrase list content.

--- javascript.js
+++ javascript.js
@@ -1,8 +1,8 @@
 let i = 0;
 let speed = 50;
-let txt = "first ";
+let txt = "initializing hackerman.exe\n";
 
-const phrases = ["code.exe running within req params ", "if not", "then() ", "hacking,x running."];
+const phrases = ["running code.exe", "injecting mainframe procedures", "finding root access...", "terminal entry granted", ];
 
 function getRandomPhrase() {
     let rand = Math.floor(Math.random() * phrases.length);
@@ -11,11 +11,12 @@ function getRandomPhrase() {
 
 function typeWriter() {
     if (i < txt.length) {
-        document.querySelector("p").textContent += txt.charAt(i);
+        document.querySelector("p").innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
     else {
+        document.querySelector("p").innerHTML += "<br>";
         i = 0;
         txt = getRandomPhrase()
     }
